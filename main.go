841ba8f588b224d29446ab92e8b0f21ace4ca480
horvatic <shorvatic@gmail.com>
2020-06-10 02:23:21 +0000

    remove error

--- main.go
+++ main.go
@@ -15,8 +15,6 @@ func main() {
 func PipelineServer(w http.ResponseWriter, r *http.Request) {
     var g hooks.GitHook
 
-    // Try to decode the request body into the struct. If there is an error,
-    // respond to the client with the error message and a 400 status code.
     err := json.NewDecoder(r.Body).Decode(&g)
     if err != nil {
 	fmt.Println("Non push request made")
