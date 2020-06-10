9d64d4f6dd02499a1739baa20aef03844d944834
horvatic <shorvatic@gmail.com>
2020-06-10 02:02:56 +0000

    add git model

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
     "fmt"
     "net/http"
+    "encoding/json"
+    "sicusan-cijev/hooks"
 )
 
 func main() {
@@ -11,5 +13,14 @@ func main() {
 }
 
 func PipelineServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(r.Body)
+    var g hooks.GitHook
+
+    // Try to decode the request body into the struct. If there is an error,
+    // respond to the client with the error message and a 400 status code.
+    err := json.NewDecoder(r.Body).Decode(&g)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+    fmt.Println(g)
 }
