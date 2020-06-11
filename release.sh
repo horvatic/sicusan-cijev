07b9ba2c518d405312028a8d16bbde4bb6d21e9b
horvatic <sbhorvatic@gmail.com>
2020-06-11 01:39:00 +0000

    Add end by pid

--- release.sh
+++ release.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-SERVICE="$2-userprocess"
+REPO_URI="$1"
+SERVICE="$2"
 
 if [ -d "$HOME/go/src/$SERVICE" ]; then
+	CURRENT_PID=$(cat "$HOME/go/src/$SERVICE/.pid.info")
+	echo "$CURRENT_PID" >> "$HOME/log.txt"
+	kill -9 "$CURRENT_PID"
 	rm -rf "$HOME/go/src/$SERVICE"
-	killall -9 "$SERVICE"
 fi
 
-git clone "$1" "$HOME/go/src/$SERVICE"
+git clone "$REPO_URI" "$HOME/go/src/$SERVICE"
 cd "$HOME/go/src/$SERVICE"
 go build main.go
 mv main "$SERVICE"
 chmod +x "$SERVICE"
 ./"$SERVICE" &
-
+echo "$!" >> "$HOME/go/src/$SERVICE/.pid.info"
